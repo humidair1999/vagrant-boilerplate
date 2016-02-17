7cbeb331c849fb952ab06543bdf2fb7bfade77e2
J. Ky Marsh <jkymarsh@gmail.com>
2016-02-16 23:45:12 -0800

    add package install to provision script

--- provision.sh
+++ provision.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-echo "lol install"
+echo "----------------------------------------------------------"
+echo "update existing apt-get packages"
+
+# sudo apt-get update
+
+echo "----------------------------------------------------------"
+echo "installing webdev apt-get packages"
+
+function install_package_if_needed() {
+	# http://askubuntu.com/a/140574
+	dpkg -s "$1" >/dev/null 2>&1 && {
+        echo "$1 is already installed."
+    } || {
+        echo "$1 is not installed. installing now"
+
+        sudo apt-get install -y $1
+    }
+}
+
+install_package_if_needed curl
+
+echo "----------------------------------------------------------"
+echo "installing nvm"
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+# source and reload nvm
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
